6bebec1d309012fe627dad7aec07fce392eb3ebf
Richard <programmingislife23@gmail.com>
2024-02-21 16:51:07 -0300

    termiando e aprendendo

---
+++ app.js
@@ -0,0 +1,30 @@
+function adicionandoApostas() {
+  const nome = prompt("Qual o seu nome? ");
+  const aposta = parseFloat(prompt("Quanto você quer apostar? "));
+  apostadores[nome] = aposta;
+}
+function verificandoVencedor(aposta) {
+  maiorAposta = 0;
+  vencedor = "";
+  for (let nome in apostadores) {
+    if (aposta[nome] > maiorAposta) {
+      maiorAposta = aposta[nome];
+      vencedor = nome;
+    }
+  }
+  console.log(`O vencedor foi ${vencedor}, com a aposta de ${maiorAposta}.`);
+}
+
+const apostadores = {};
+adicionandoApostas();
+let programaOn = true;
+console.log(apostadores[apostadores]);
+while (programaOn == true) {
+  const maisApostas = prompt("Tem mais apostadores? ").toLowerCase();
+  if (maisApostas == "sim" || maisApostas == "s") {
+    adicionandoApostas();
+  } else {
+    verificandoVencedor(apostadores);
+    programaOn = false;
+  }
+}
